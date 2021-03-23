d8dc1b8e6a8643f485bf40cd86def9761d889881
RemiBudi <62890401+RemiBudi@users.noreply.github.com>
2021-03-23 23:56:37 +0100

    Add files via upload

---
+++ src/main/java/com/Places/dao/User.java
@@ -0,0 +1,69 @@
+package com.Places.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jdo.annotations.PersistenceCapable;
+
+@PersistenceCapable
+public class User {
+
+	String userName;
+	List<User> contacts = new ArrayList<User>();
+	List<Map> myMaps;
+	List<Comment> myComments;
+
+	// Constructeur
+	public User(String userName, List<User> contacts, List<Map> myMaps, List<Comment> myComments) {
+		this.userName = userName;
+		this.contacts = contacts;
+		this.myMaps = myMaps;
+		this.myComments = myComments;
+
+	}
+	
+	//constructeur username
+	public User(String un) {
+		this.userName = un;
+	}
+
+	// Constructeur vide
+	public User() {
+
+	}
+
+	// Getter
+	public String getUserName() {
+		return this.userName;
+	}
+
+	public List<User> getContacts() {
+		return this.contacts;
+	}
+
+	public List<Map> getMyMaps() {
+		return this.myMaps;
+	}
+
+	public List<Comment> getMyComments() {
+		return this.myComments;
+	}
+
+	// Setter
+	public void setUserName(String u) {
+		this.userName = u;
+	}
+
+	public void setContacts(List<User> us) {
+		this.contacts = us;
+	}
+
+	public void setMyMaps(List<Map> m) {
+		this.myMaps = m;
+	}
+
+	public void setMyComments(List<Comment> c) {
+		this.myComments = c;
+	}
+
+}
