ed307994d25deee810c267328a2e015284ffaa2d
RemiBudi <62890401+RemiBudi@users.noreply.github.com>
2021-03-28 13:48:16 +0200

    Add files via upload

---
+++ src/test/java/datanucleus/MapContainerTest.java
@@ -0,0 +1,51 @@
+package datanucleus;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.Places.dao.Map;
+import com.Places.dao.MapContainer;
+
+public class MapContainerTest {
+	
+	@Test
+	public void test() {
+		PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("Example");
+		Long containerId = null;
+		
+		{
+			PersistenceManager pm = pmf.getPersistenceManager();
+
+			Map map1 = new Map();
+			Map map2 = new Map();
+			Map map3 = new Map();
+			map1.setTitle("map1");
+			map2.setTitle("map2");
+			map3.setTitle("map3");
+
+			MapContainer container = new MapContainer();
+			container.getMaps().add(map1);
+			container.getMaps().add(map2);
+			container.getMaps().add(map3);
+
+			container = pm.makePersistent(container);
+			containerId = container.getId();
+			pm.close();
+		}
+		
+		{
+			PersistenceManager pm = pmf.getPersistenceManager();
+
+			MapContainer container = pm.getObjectById(MapContainer.class, containerId);
+			Assert.assertEquals(3, container.getMaps().size());
+
+			pm.close();
+		}
+		pmf.close();
+	}
+
+}
