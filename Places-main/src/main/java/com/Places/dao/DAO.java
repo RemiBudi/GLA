d1d5d9e2fcd868a1607ca94fbfb54131b28e2280
RemiBudi <62890401+RemiBudi@users.noreply.github.com>
2021-03-31 10:09:14 +0200

    Add files via upload

---
+++ Places-main/src/main/java/com/Places/dao/DAO.java
@@ -0,0 +1,17 @@
+package com.Places.dao;
+
+public class DAO{
+
+	// Comments methods
+	
+
+	// Map methods
+
+	
+
+	// Pin methods
+	
+	// User Dao
+	
+
+}
---
+++ Places-main/src/main/java/com/Places/dao/UserContainer.java
@@ -0,0 +1,43 @@
+package com.Places.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+
+@PersistenceCapable
+public class UserContainer {
+	
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.NATIVE)
+	protected Long id = null;
+
+	@Persistent
+	protected List<User> users = null;
+
+	public UserContainer() {
+		super();
+		this.users = new ArrayList<User>();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> us) {
+		this.users = us;
+	}
+
+}
